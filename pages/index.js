a71e770fe92f815e05b50882ba9c197fe53d06fd
rober <me@rober.dev>
2020-08-18 09:53:56 +0200

    HomePage working fine

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// Vendor libs
+import React from 'react';
+
+// Component definition
+const HomePage = () => {
+  return (
+    <>
+      <h1>Hello world !!!</h1>
+    </>
+  );
+};
+
+// Exportation
+export default HomePage;
